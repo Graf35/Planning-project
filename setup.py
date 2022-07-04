549513080b6b531d73cf2ecf51b38518405712b3
Graf_35 <Graf3456718@yandex.ru>
2022-07-04 23:44:27 +0300

    Version 0.3

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ base = None
 if sys.platform == 'win32':
     base = "Win32GUI"
 
-executables = [cx_Freeze.Executable("main.py", base=base, icon = "icon.ico")]
-build_exe_options = {"includes":["Main_window"]
+executables = [cx_Freeze.Executable("main.py", base=base, icon = "icon.ico", )]
+build_exe_options = {"includes":["Main_window", "monthly_volume_planning", "Script"],
+                     "py-modules":[]
                      }
 
 cx_Freeze.setup(
     name = "Planning-project",
     options = {"build_exe": build_exe_options},
-    version = "0.1",
+    version = "0.3",
     description = "A demo version of the repair planning system",
-    executables = executables
+    executables = executables,
+
+
     )
